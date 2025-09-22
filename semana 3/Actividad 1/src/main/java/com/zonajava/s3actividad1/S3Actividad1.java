7b920a65164c89294b6e010bcb4a339e9961803b
Jhovany2 <t01260h@ms.upla.edu.pe>
2025-09-21 21:35:29 -0500

    Add files via upload

---
+++ semana 3/Actividad 1/src/main/java/com/zonajava/s3actividad1/S3Actividad1.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.zonajava.s3actividad1;
+
+import vista.AplicaOferta;
+/**
+ *
+ * @author LENOVO
+ */
+public class S3Actividad1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+          java.awt.EventQueue.invokeLater(new Runnable() {
+        public void run() {
+            new AplicaOferta().setVisible(true);
+        }
+    });
+
+    }
+}
